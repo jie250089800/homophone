6360181858467f2150660a21cd054ce29d6caec8
huijieye <250089800@qq.com>
2020-06-27 03:50:21 +0800

    update sql

---
+++ SQL/homophone_table_sheng_diao_sort.sql
@@ -0,0 +1,5 @@
+INSERT INTO homophone_table.sheng_diao_sort (id, sheng_diao, sort) VALUES (1, '51', 1);
+INSERT INTO homophone_table.sheng_diao_sort (id, sheng_diao, sort) VALUES (2, '24', 2);
+INSERT INTO homophone_table.sheng_diao_sort (id, sheng_diao, sort) VALUES (3, '33', 3);
+INSERT INTO homophone_table.sheng_diao_sort (id, sheng_diao, sort) VALUES (4, '21', 4);
+INSERT INTO homophone_table.sheng_diao_sort (id, sheng_diao, sort) VALUES (5, '55', 5);
